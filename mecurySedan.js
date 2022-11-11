7524a6fcefcbea154b4969496fdacba4d4adce21
samanthacornier <samantha112069@gmail.com>
2022-11-11 15:02:26 -0500

    redo assignment

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,2 +1,28 @@
-class vehicle {
-    
+class Vehicle {
+    //entered constructor with properties
+    constructor(make, model, year, color,) {
+        this.model = model;
+        this.year = year;
+        this.make = make;
+        this.numberOfWheels = 0;
+        this.color = color;
+        this.passenger = 0;
+        this.speed = 0;
+        this.started = false;
+        this.fuel = 10;
+        this.scheduleService = false}
+
+        module.exports = { Vehicle }
+        
+        start {if (this.fuel > 0) {
+                return this.started = true;}
+                console.log("no fuel");
+        checkService() {
+                if (this.mileage > 35000) {            
+                this.scheduleService = true
+
+        loadPassenger(num) {
+            if (this.passenger < this.maxPassengers)
+            console.log(" not have enough space to take all passengers.")
+            
+            let myCar = new Car('mercury', 'sedan', '2005', 'blue', 40000)
